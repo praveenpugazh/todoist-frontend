ff4709c4b99539214745086541fbcc5d632c2dfb
praveen pugazh <praveenpugazh14@gmail.com>
2024-03-28 11:37:09 +0530

    added rerouting login to register and vice versa

--- src/components/ui/LoginRegCard.jsx
+++ src/components/ui/LoginRegCard.jsx
@@ -12,6 +12,9 @@ import axios from 'axios'
 import { API_URL } from '@/lib/constants'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Toaster } from '@/components/ui/toaster'
+import { useToast } from '@/components/ui/use-toast'
+
 // eslint-disable-next-line react/prop-types
 export function LoginRegCard({ action }) {
   const [postData, setPostData] = useState({
@@ -33,6 +36,8 @@ export function LoginRegCard({ action }) {
       navigate('/')
     }
   }, [isLoggedIn, navigate])
+  const { toast } = useToast()
+
   const loginHandler = async () => {
     try {
       const { data } = await axios.post(`${API_URL}/user/login`, {
@@ -41,74 +46,105 @@ export function LoginRegCard({ action }) {
       })
       localStorage.setItem('auth', data.accessToken)
       setIsLoggedIn(true)
+
+      toast({
+        variant: 'success',
+        title: 'Success',
+        description: 'Login successful'
+      })
     } catch (error) {
       console.log(error)
+      setPostData({
+        ...postData,
+        password: ''
+      })
+      toast({
+        variant: 'destructive',
+        title: 'Failed',
+        description: 'Login failed, Please try again'
+      })
     }
   }
   const registerHandler = async () => {
     try {
       const { data } = await axios.post(`${API_URL}/user/register`, postData)
+      toast({
+        variant: 'success',
+        title: 'Success',
+        description: 'Login successful'
+      })
       console.log(data)
+      navigate('/login')
     } catch (error) {
       console.log(error)
+      toast({
+        variant: 'destructive',
+        title: 'Failed',
+        description: 'Register unsuccessful'
+      })
     }
   }
   return (
-    <Card className='w-[380px] md:w-[500px]'>
-      <CardHeader>
-        <CardTitle>{action === 'register' ? 'Register' : 'Login'}</CardTitle>
-        <CardDescription>
-          {action === 'register'
-            ? 'Register below to continue'
-            : 'Login below to continue'}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className='flex flex-col space-y-4'>
-        {action === 'register' ? (
+    <>
+      <Card className='w-[380px] md:w-[500px]'>
+        <CardHeader>
+          <CardTitle>{action === 'register' ? 'Register' : 'Login'}</CardTitle>
+          <CardDescription>
+            {action === 'register'
+              ? 'Register below to continue'
+              : 'Login below to continue'}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='flex flex-col space-y-4'>
+          {action === 'register' ? (
+            <Input
+              type='text'
+              placeholder='Name'
+              onChange={(e) =>
+                setPostData({ ...postData, name: e.target.value })
+              }
+            />
+          ) : (
+            <></>
+          )}
           <Input
-            type='text'
-            placeholder='Name'
-            onChange={(e) => setPostData({ ...postData, name: e.target.value })}
+            type='email'
+            placeholder='Email'
+            onChange={(e) =>
+              setPostData({ ...postData, email: e.target.value })
+            }
           />
-        ) : (
-          <></>
-        )}
-        <Input
-          type='email'
-          placeholder='Email'
-          onChange={(e) => setPostData({ ...postData, email: e.target.value })}
-        />
-        <Input
-          type='password'
-          placeholder='Password'
-          onChange={(e) =>
-            setPostData({ ...postData, password: e.target.value })
-          }
-        />
-        <Button
-          onClick={() => {
-            console.log('clicked')
-            action === 'register' ? registerHandler() : loginHandler()
-          }}
-        >
-          {action === 'register' ? 'Register' : 'Login'}
-        </Button>
-      </CardContent>
+          <Input
+            type='password'
+            placeholder='Password'
+            onChange={(e) =>
+              setPostData({ ...postData, password: e.target.value })
+            }
+          />
+          <Button
+            onClick={() => {
+              console.log('clicked')
+              action === 'register' ? registerHandler() : loginHandler()
+            }}
+          >
+            {action === 'register' ? 'Register' : 'Login'}
+          </Button>
+        </CardContent>
 
-      <CardFooter>
-        {action !== 'register' ? (
-          <p>
-            Don&apos;t have an account? <a href='/register'>Register</a>
-          </p>
-        ) : (
-          <p>
-            Have an account already? <a href='/login'>Login</a>
-          </p>
-        )}
-      </CardFooter>
-
-      <></>
-    </Card>
+        <CardFooter>
+          {action !== 'register' ? (
+            <p>
+              Don&apos;t have an account? <a href='/register'>Register</a>
+            </p>
+          ) : (
+            <p>
+              Have an account already? <a href='/login'>Login</a>
+            </p>
+          )}
+        </CardFooter>
+      </Card>
+      <Toaster />
+    </>
   )
 }
 
